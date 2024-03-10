a444a5111a81af2780d71acb95634d892990ae9f
MiriamPaternain <miriampaternain@gmail.com>
2024-03-10 18:22:24 +0100

    add template react

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+//seccion import
+
+//.- de React, de archivos propios, Sass, Images
+import '../styles/App.scss';
+
+/*  COMPONENTE */
+function App() {
+  /*
+    Variables de estado, funciones manejadoras de eventos, variables, funcion handle 
+  */
+  /* RETURN --> HTML */
+  return <div>pagina principal</div>;
+}
+
+/* export*/
+export default App;

5edc71cc8b7dbfac90ec8111e3772856666659aa
MiriamPaternain <miriampaternain@gmail.com>
2024-03-11 20:06:01 +0100

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,18 @@
+import logoLarge from '../img/bookWiserLogoPurpleLarge.png';
+
 function Footer() {
-  return <></>;
+  return (
+    <>
+      <div className='footerContainer'>
+        <img src={logoLarge} alt='' className='footerContainer_logo' />
+        <ul className='footerContainer_list'>
+          <li className='footerContainer_list--item'>Sobre bookWiser</li>
+          <li className='footerContainer_list--item'>Mi cuenta</li>
+          <li className='footerContainer_list--item'>Crear cuenta</li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Footer;

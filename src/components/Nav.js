7ce7c4c5bde4639f2bcaa5aff5127da0cf7976a4
MiriamPaternain <miriampaternain@gmail.com>
2024-03-11 12:36:40 +0100

    add searcher to nav-mobile

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -14,18 +14,30 @@ function Nav() {
 
   return (
     <>
-      <div className='navContainer'>
-        <img src={logo} alt='' className='navContainer_logo' />
-        <input
-          type='text'
-          placeholder='Autor / Título '
-          className='navContainer_input'
-        />
-        <button className='navContainer_btn' onClick={toggleMenu}>
-          {menuOpen ? <IoMdClose /> : <TfiAlignCenter />}
-        </button>
+      <div className='nav'>
+        <div className='navContainer'>
+          <img src={logo} alt='' className='navContainer_logo' />
+          <input
+            type='text'
+            placeholder='Autor / Título '
+            className='navContainer_input'
+          />
+          <button className='navContainer_btn' onClick={toggleMenu}>
+            {menuOpen ? <IoMdClose /> : <TfiAlignCenter />}
+          </button>
+        </div>
+        {menuOpen && <Menu onClose={toggleMenu} />}
+        <div className='nav_search'>
+          <input
+            type='text'
+            placeholder='Autor / Título '
+            className='nav_search--input'
+          />
+          <button className='nav_search--icon'>
+            <IoSearchSharp />
+          </button>
+        </div>
       </div>
-      {menuOpen && <Menu onClose={toggleMenu} />}
     </>
   );
 }

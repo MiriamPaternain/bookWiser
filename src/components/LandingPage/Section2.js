702818dee190ca48123fbab1f2cb2461087aef19
MiriamPaternain <miriampaternain@gmail.com>
2024-03-11 18:34:47 +0100

    add section2 animation

--- src/components/LandingPage/Section2.js
+++ src/components/LandingPage/Section2.js
@@ -1,8 +1,46 @@
+import { useEffect, useState } from 'react';
+
 function Section2() {
+  const [typedText, setTypedText] = useState('');
+  const text = 'Únete a bookWiser:';
+  const typingSpeed = 250;
+  const pauseAfterComplete = 1500;
+
+  useEffect(() => {
+    let currentIndex = 0;
+
+    const typingInterval = setInterval(() => {
+      if (currentIndex >= text.length) {
+        clearInterval(typingInterval);
+        setTimeout(() => {
+          currentIndex = 0;
+          const loopInterval = setInterval(() => {
+            setTypedText((prevTypedText) => {
+              if (currentIndex >= text.length) {
+                clearInterval(loopInterval);
+                return prevTypedText;
+              } else {
+                currentIndex++;
+                return text.substring(0, currentIndex);
+              }
+            });
+          }, typingSpeed);
+        }, pauseAfterComplete);
+      } else {
+        setTypedText((prevTypedText) => {
+          currentIndex++;
+          return text.substring(0, currentIndex);
+        });
+      }
+    }, typingSpeed);
+
+    return () => clearInterval(typingInterval);
+  }, []);
+
   return (
     <>
       <div className='section2Container'>
-        <p className='section2Container_join'>Únete a bookWiser:</p>
+        <p className='section2Container_join'>{typedText}</p>
         <div className='section2Container_account'>
           <input
             type='text'

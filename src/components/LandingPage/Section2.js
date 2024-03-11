7816fbaa83dd49438bdc3193d89ad45ef48c63cd
MiriamPaternain <miriampaternain@gmail.com>
2024-03-11 13:30:41 +0100

    add section2

--- src/components/LandingPage/Section2.js
+++ src/components/LandingPage/Section2.js
@@ -1,5 +1,22 @@
 function Section2() {
-  return <></>;
+  return (
+    <>
+      <div className='section2Container'>
+        <p className='section2Container_join'>Únete a bookWiser:</p>
+        <div className='section2Container_account'>
+          <input type='text' className='section2Container_account--user' />
+          <input type='text' className='section2Conatiner_account--password' />
+        </div>
+        <button className='section2Container_btn'>Crear cuenta</button>
+        <p className='section2Container_logIn'>
+          ¿Ya tienes cuenta?{' '}
+          <a href='' className='section2Container_LogIn--link'>
+            Log in
+          </a>
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default Section2;

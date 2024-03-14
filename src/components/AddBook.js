5c1922bc8dc7bca524b54b0655b70865046d2fda
MiriamPaternain <miriampaternain@gmail.com>
2024-03-14 11:50:02 +0100

    add logic to addbook form

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,7 +1,40 @@
 import Footer from './Footer';
 import Nav from './Nav';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function AddBook() {
+  const [bookData, setBookData] = useState({
+    title: '',
+    author: '',
+    genre: '',
+    imageUrl: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setBookData({ ...bookData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('/api/books', bookData);
+
+      setBookData({
+        title: '',
+        author: '',
+        genre: '',
+        imageUrl: '',
+      });
+      alert('Libro añadido correctamente');
+    } catch (error) {
+      console.error('Error al añadir el libro:', error);
+      alert(
+        'Ocurrió un error al añadir el libro. Por favor, intenta de nuevo más tarde'
+      );
+    }
+  };
   return (
     <>
       <Nav showNavSearch={false} />
@@ -21,21 +54,37 @@ function AddBook() {
             type='text'
             className='addBookContainer_form--input'
             placeholder='Título del libro'
+            name='title'
+            value={bookData.title}
+            onChange={handleChange}
+            required
           />
           <input
             type='text'
             className='addBookContainer_form--input'
             placeholder='Autor/a'
+            name='author'
+            value={bookData.author}
+            onChange={handleChange}
+            required
           />
           <input
             type='text'
             className='addBookContainer_form--input'
             placeholder='Género'
+            name='genre'
+            value={bookData.genre}
+            onChange={handleChange}
+            required
           />
           <input
             type='text'
             className='addBookContainer_form--input'
             placeholder='* Enlace de la imagen'
+            name='imageUrl'
+            value={bookData.imageUrl}
+            onChange={handleChange}
+            required
           />
           <p className='addBookContainer_form--imgLinkText'>
             * busca una imagen de la portada en google, copia el enlace y pégalo
